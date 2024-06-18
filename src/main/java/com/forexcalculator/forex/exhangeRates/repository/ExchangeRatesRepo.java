526733b6d3f7abca8c902df6ec87b338ec50bcef
brianmuli <brianmulimuteti@gmail.com>
2024-06-18 09:05:11 +0300

    added transaction module

--- src/main/java/com/forexcalculator/forex/exhangeRates/repository/ExchangeRatesRepo.java
+++ src/main/java/com/forexcalculator/forex/exhangeRates/repository/ExchangeRatesRepo.java
@@ -4,9 +4,11 @@ import com.forexcalculator.forex.currencyPair.entity.CurrencyPair;
 import com.forexcalculator.forex.exhangeRates.entity.ExchangeRates;
 import com.forexcalculator.forex.forexBureau.entity.ForexBureau;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ExchangeRatesRepo  extends JpaRepository<ExchangeRates, Long> {
     List<ExchangeRates> findByForexBureau(ForexBureau forexBureau);
     List<ExchangeRates> findByCurrencyPair(CurrencyPair currencyPair);
--- src/main/java/com/forexcalculator/forex/forexBureau/repository/ForexRepo.java
+++ src/main/java/com/forexcalculator/forex/forexBureau/repository/ForexRepo.java
@@ -2,10 +2,12 @@ package com.forexcalculator.forex.forexBureau.repository;
 
 import com.forexcalculator.forex.forexBureau.entity.ForexBureau;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface ForexRepo extends JpaRepository<ForexBureau, Long> {
     Optional<ForexBureau> findByBureauName(String bureauName);
     List<ForexBureau> findByLocation(String location);
